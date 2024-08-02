1278e5448ad72a5f5ee716918a18dc3dbc2b8536
Daniela Catelan <82057219+danielacatelan@users.noreply.github.com>
2024-08-02 16:34:26 -0400

    divx.h

--- Approx_Instructions/divx.h
+++ Approx_Instructions/divx.h
@@ -1,5 +1,21 @@
-//MULX COM TRUNCAMENTO
-reg_t rs1 = (RS1 >> 6) << 6;
-reg_t rs2 = (RS2 >> 6) << 6;
+//DIVX 2X1 - APSC4
 
-WRITE_RD(sext_xlen(rs1 * rs2));
+reg_t Bin = 0;
+reg_t Q = 0;
+
+//P0
+reg_t mask1 = 1;
+reg_t mask2 = 2;
+
+reg_t x0 = (RS1 & mask1);
+reg_t y   = (RS2 & mask1);
+
+reg_t Bout = ((~(x0 ^ (y ^ Q)) & Bin) | (~x0 & (y ^ Q)));
+
+reg_t R = ((~x0 & (y ^ Q)) | ((y ^ Q) & Cin) | (~x0 & Cin) | (x0 & ~(y ^ Q) & ~Cin));
+
+reg_t x1 = (RS1 & mask2);
+
+reg_t Quoc = x1 ^ Bout ^ Q;
+
+WRITE_RD(sext_xlen(Quoc));
