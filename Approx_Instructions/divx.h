cbf7aa3f3f25667ddfdb8daa76fca4461bbafa01
Daniela Catelan <82057219+danielacatelan@users.noreply.github.com>
2026-06-24 07:28:32 -0400

    divx.h

--- Approx_Instructions/divx.h
+++ Approx_Instructions/divx.h
@@ -1,6 +1,11 @@
 //Divx - 64X32
 //Line 0 - Quoc 31
 
+//NÃO FINALIZADA
+
+WRITE_RD(sext_xlen(RS1 ^ RS2));
+
+/*
 //Block 0 
 reg_t Bin = 0;
 reg_t Q = 0;
@@ -40,3 +45,4 @@ reg_t Quoc0 = ~(R991 ^ Bout1023 ^ Q);
 Quoc = ((Quoc31) | (Quoc30) | (Quoc29) | (Quoc28) | (Quoc27) | (Quoc26) | (Quoc25) | (Quoc24) | (Quoc23) | (Quoc22) | (Quoc21) | (Quoc20) | (Quoc19) | (Quoc18) | (Quoc17) | (Quoc16) | (Quoc15) | (Quoc14) | (Quoc13) | (Quoc12) | (Quoc11) | (Quoc10) | (Quoc9) | (Quoc8) | (Quoc7) | (Quoc6) | (Quoc5) | (Quoc4) | (Quoc3) | (Quoc2) | (Quoc1) | (Quoc0));
 
 WRITE_RD(sext_xlen(Quoc));
+*/
